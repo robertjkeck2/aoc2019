68792d61c1025e0cbe1d189b1b0de5ab22f42ee0
John Keck <robertjkeck2@gmail.com>
2019-12-02 13:33:58 -0500

    finished day2 part 2

--- day2/day2.go
+++ day2/day2.go
@@ -9,16 +9,10 @@ import (
 
 func main() {
 	filename := "opcodes.csv"
-	opcodes := loadOpcodes(filename)
-	for i := 0; i < len(opcodes); i = i + 4 {
-		value := evaluateOpcode(opcodes[i], opcodes[i+1], opcodes[i+2], opcodes)
-		if value == -1 {
-			break
-		} else {
-			opcodes[opcodes[i+3]] = value
-		}
-	}
-	fmt.Println(opcodes[0])
+	searchNum := int64(19690720)
+	originalOpcodes := loadOpcodes(filename)
+	noun, verb := searchInputs(originalOpcodes, searchNum)
+	fmt.Println(100*noun + verb)
 }
 
 func loadOpcodes(filename string) (opcodes []int64) {
@@ -51,4 +45,36 @@ func evaluateOpcode(op, pos1, pos2 int64, opcodes []int64) (value int64) {
 		value = -1
 	}
 	return value
+}
+
+func runProgram(originalOpcodes []int64, noun, verb int64) (output int64) {
+	opcodes := make([]int64, len(originalOpcodes))
+	copy(opcodes, originalOpcodes)
+	opcodes[1] = noun
+	opcodes[2] = verb
+	for i := 0; i < len(opcodes); i = i + 4 {
+		value := evaluateOpcode(opcodes[i], opcodes[i+1], opcodes[i+2], opcodes)
+		if value == -1 {
+			break
+		} else {
+			opcodes[opcodes[i+3]] = value
+		}
+	}
+	output = opcodes[0]
+	return output
+}
+
+func searchInputs(originalOpcodes []int64, searchNum int64) (noun, verb int64) {
+	var i, j int64
+	for i = 0; i < 100; i++ {
+		for j = 0; j < 100; j++ {
+			output := runProgram(originalOpcodes, i, j)
+			if output == searchNum {
+				noun = i
+				verb = j
+				break
+			}
+		}
+	}
+	return noun, verb
 }
