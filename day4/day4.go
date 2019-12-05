708267cd343fb6a8af200bcc9f5793e11d6e151a
John Keck <robertjkeck2@gmail.com>
2019-12-04 23:25:24 -0500

    finished day4 part 2

--- day4/day4.go
+++ day4/day4.go
@@ -39,14 +39,33 @@ func countPossibilities(pw pwRange) (count int) {
 func checkCritera(value int) (valid bool) {
 	valid = true
 	strValue := strconv.Itoa(value)
+	counts := make(map[int]int)
 	if value-100000 < 0 {
 		valid = false
+		return
 	}
 	if strValue[0] != strValue[1] && strValue[1] != strValue[2] && strValue[2] != strValue[3] && strValue[3] != strValue[4] && strValue[4] != strValue[5] {
 		valid = false
+		return
 	}
 	if strValue[0] > strValue[1] || strValue[1] > strValue[2] || strValue[2] > strValue[3] || strValue[3] > strValue[4] || strValue[4] > strValue[5] {
 		valid = false
+		return
+	}
+	for j := 48; j < 58; j++ {
+		for i := 1; i < 6; i++ {
+			if int(strValue[i]) == j && strValue[i-1] == strValue[i] {
+				counts[j]++
+			}
+		}
+	}
+	if len(counts) == 1 {
+		for _, val := range counts {
+			if val > 1 {
+				valid = false
+				return
+			}
+		}
 	}
 	return
 }
